4b956fb4e57d8cf8a2b7770d8f8fac1d7cccc21e
Josip Kuštan <jkustan@croz.net>
2023-02-27 10:35:20 +0100

    Komponenta informacije Dodatni podaci

---
+++ src/components/shared/InfoTag.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const InfoTag = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 8px;
+  gap: 4px;
+  background: rgba(255, 255, 255, 0.65);
+  backdrop-filter: blur(5px);
+  border-radius: 16px 0px 0px 16px;
+`;
